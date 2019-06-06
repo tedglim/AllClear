10973996f74b1579d2280d3210715c2b43e522e1
tedglim <tedlim@alumni.stanford.edu>
2019-06-06 00:41:02 -0700

    init board done

---
+++ Match3/Assets/Scripts/BoardManagerScript.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManagerScript : MonoBehaviour
+{
+    public int boardX = 5;
+    public int boardY = 5;
+
+    private GameObject prevGem;
+    private GameObject[,] boardLayout;
+    public List<GameObject> gemPool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitBoard();
+        
+    }
+
+    private void InitBoard()
+    {
+        boardLayout = new GameObject[boardX, boardY];
+        GameObject prevGem = null;
+        GameObject[] prevRowGems = new GameObject[boardX];
+        for (int y = 0; y < boardY; y++)
+        {
+            for (int x = 0; x < boardX; x++)
+            {
+                List<GameObject> availableGems = new List<GameObject>();
+                availableGems.AddRange(gemPool);
+                availableGems.Remove(prevGem);
+                availableGems.Remove(prevRowGems[x]);
+                prevGem = availableGems[UnityEngine.Random.Range(0, availableGems.Count)];
+                prevRowGems[x] = prevGem;
+                boardLayout[x, y] = (GameObject)Instantiate(prevGem, new Vector2(x - 2, y - 2), Quaternion.identity);
+                boardLayout[x,y].transform.parent = transform;
+                // boardLayout.Ad
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Match3/Assets/Scripts/SwipeToMoveScript.cs
+++ Match3/Assets/Scripts/SwipeToMoveScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SwipeToMoveScript : MonoBehaviour
 {
+    private Vector2 startTouchPos, endTouchPos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,25 @@ public class SwipeToMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTouchPos = Input.GetTouch(0).position;
+        }
+
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            endTouchPos = Input.GetTouch(0).position;
+
+            if((endTouchPos.x < startTouchPos.x) && transform.position.x > -1.75f)
+            {
+                transform.position = new Vector2(transform.position.x - 1.75f, transform.position.y);
+            }
+
+            if((endTouchPos.x > startTouchPos.x) && transform.position.x < 1.75f)
+            {
+                transform.position = new Vector2(transform.position.x + 1.75f, transform.position.y);
+            }
+        }
+
     }
 }
